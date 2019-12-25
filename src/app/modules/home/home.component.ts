5d17fa797bb96ead4e499a5dfd0a3997ac1ae641
Sujith M A <sujithma@taxbackinternational.com>
2019-12-25 11:25:52 +0530

    thread design

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -11,12 +11,34 @@ export class HomeComponent implements OnInit {
 	openThread = false;
 	user;
 	messages = MESSAGES;
+	selectedMsg;
 	constructor(
 		private auth: AuthService
 	) { }
 
 	ngOnInit() {
 		this.user = this.auth.user;
+	}
+
+	send(event) {
+		if (event.keyCode === 13 || event.target.keyCode === 13) {
+			const message: Message = {
+				id: Math.round(Math.random()),
+				fromUserId: this.user.id,
+				user: this.user.email,
+				thumbnail: '',
+				date: new Date().toDateString(),
+				toUserId: this.user.id,
+				message: 'hgfghg'
+			};
+
+			this.messages.push(message);
+		}
+	}
+
+	startThread(msg) {
+		this.selectedMsg = msg;
+		this.openThread = !this.openThread;
 	}
 
 }
