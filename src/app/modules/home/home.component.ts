517a7d92f20429abd7a7dbdc2d830ddeb74edb13
Sujith M A <sujithma@taxbackinternational.com>
2019-12-25 14:39:46 +0530

    reply message

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 import { AuthService } from 'src/app/core/auth/auth.service';
 import { FormControl } from '@angular/forms';
+
+let id = 0;
 
 @Component({
 	selector: 'app-home',
@@ -15,9 +17,19 @@ export class HomeComponent implements OnInit {
 	selectedMsg;
 
 	message: FormControl = new FormControl();
+	replyMessage: FormControl = new FormControl();
+
+	keyCodes = {
+		shiftkey: false,
+		enterkey: false
+	};
+
+	@ViewChild('messagesRef', { static: false }) messagesRef: ElementRef;
+	@ViewChild('messageTextAreaRef', { static: false }) messageTextAreaRef: ElementRef;
+	@ViewChild('replyMessagesRef', { static: false }) replyMessagesRef: ElementRef;
 
 	constructor(
-		private auth: AuthService
+		private auth: AuthService,
 	) { }
 
 	ngOnInit() {
@@ -25,25 +37,75 @@ export class HomeComponent implements OnInit {
 	}
 
 	send(event) {
-		if (event.keyCode === 13 || event.target.keyCode === 13) {
+		if (event.keyCode === 16) {
+			this.keyCodes.shiftkey = true;
+		}
+		if (event.keyCode === 13) {
+			this.keyCodes.enterkey = true;
+		}
+		if (!this.keyCodes.shiftkey && this.keyCodes.enterkey && this.message.value !== null && this.message.value !== '') {
 			const message: Message = {
-				id: Math.round(Math.random()),
+				id: ++id,
 				fromUserId: this.user.id,
 				user: this.user.email,
 				thumbnail: '',
 				date: new Date().toDateString(),
 				toUserId: this.user.id,
-				message: 'hgfghg'
+				message: this.message.value,
 			};
 
 			this.messages.push(message);
 			this.message.setValue('');
+
+			this.messageTextAreaRef.nativeElement.focus();
+			setTimeout(() => {
+				this.messagesRef.nativeElement.scrollTop = this.messagesRef.nativeElement.scrollHeight;
+			}, 10);
+			event.preventDefault();
 		}
 	}
 
 	startThread(msg) {
 		this.selectedMsg = msg;
-		this.openThread = !this.openThread;
+		this.openThread = true;
+	}
+
+	reply(event, msg) {
+		if (event.keyCode === 16) {
+			this.keyCodes.shiftkey = true;
+		}
+		if (event.keyCode === 13) {
+			this.keyCodes.enterkey = true;
+		}
+		if (!this.keyCodes.shiftkey && this.keyCodes.enterkey && this.replyMessage.value !== null && this.replyMessage.value !== '') {
+			const message: Message = {
+				id: ++id,
+				fromUserId: this.user.id,
+				user: this.user.email,
+				thumbnail: '',
+				date: new Date().toDateString(),
+				toUserId: msg.fromUserId,
+				message: this.replyMessage.value,
+			};
+
+			const index = this.messages.findIndex(mes => mes.id === msg.id);
+			if (index !== -1) {
+				this.messages[index].replies ? this.messages[index].replies.push(message) : this.messages[index]['replies'] = [message];
+				this.messages = this.messages.slice(0);
+			}
+			this.replyMessage.setValue('');
+			event.preventDefault();
+
+			// this.messageTextAreaRef.nativeElement.focus();
+			setTimeout(() => {
+				this.replyMessagesRef.nativeElement.scrollTop = this.replyMessagesRef.nativeElement.scrollHeight;
+			}, 10);
+		}
+	}
+
+	resetKey() {
+		this.keyCodes.shiftkey = false;
+		this.keyCodes.enterkey = false;
 	}
 
 }
@@ -57,7 +119,7 @@ export interface Message {
 	thumbnail: string;
 	date: string;
 	toUserId: number;
-	parentMessageId?: number;
+	replies?: any[];
 	message: string;
 }
 
